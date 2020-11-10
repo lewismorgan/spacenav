0b3cd0c5021764297ed9034da5b41c3606e5b242
Lewis Morgan <lewismorgan1995@icloud.com>
2020-11-10 18:50:30 -0500

    Add expansion to table row if launch details exist

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -9,12 +9,13 @@ function createData(
   rocket: string,
   capsule: string,
   launchpad: string,
-  state: string
+  state: string,
+  details: string
 ) {
-  return { name, date, capsule, rocket, launchpad, state };
+  return { name, date, capsule, rocket, launchpad, state, details };
 }
 
-// TODO: Load rows from the SpaceX API
+// TODO: Load from the SpaceX API
 const tempRows: UpcomingLaunch[] = [
   createData(
     "Crew-1", // launches/upcoming (name)
@@ -22,7 +23,8 @@ const tempRows: UpcomingLaunch[] = [
     "Dragon 2.0", // capsules/5f6f99fddcfdf403df379709 (type)
     "Falcon 9", // rockets/5e9d0d95eda69973a809d1ec (name)
     "KSC LC 39A", // launchpads/5e9e4502f509094188566f88 (name)
-    "Florida, Cape Canaveral" // launchpads/5e9e4502f509094188566f88 (region, locality)
+    "Florida, Cape Canaveral", // launchpads/5e9e4502f509094188566f88 (region, locality)
+    "SpaceX will launch the first operational mission of its Crew Dragon vehicle as part of NASA's Commercial Crew Transportation Capability Program (CCtCap), carrying 3 NASA astronauts and 1 JAXA astronaut to the International Space Station. This mission will be the second crewed flight to launch from the United States since the end of the Space Shuttle program in 2011."
   ),
 ];
 
--- src/Components/Upcoming/Upcoming.tsx
+++ src/Components/Upcoming/Upcoming.tsx
@@ -1,4 +1,7 @@
 import {
+  Box,
+  Collapse,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -7,15 +10,19 @@ import {
   TableHead,
   TableRow,
 } from "@material-ui/core";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
 import React from "react";
 
 export interface UpcomingLaunch {
   name: string;
+  details?: string;
   date: number;
   capsule: string;
   rocket: string;
   launchpad: string;
   state: string;
+  patchUrl?: string;
+  crew?: string[];
 }
 
 interface UpcomingLaunchesProps {
@@ -32,13 +39,14 @@ interface UpcomingLaunchesProps {
 const UpcomingLaunches: React.FunctionComponent<UpcomingLaunchesProps> = (
   props
 ) => {
-  // TODO: Provide the ability to expand the details of a launch if it has some
-  //      for example: when you expand the Crew-1 launch, it should give the mission patch, crew, and details
+  const { upcoming } = props;
   return (
     <TableContainer component={Paper}>
-      <Table>
+      <Table aria-label="table">
         <TableHead>
           <TableRow>
+            <TableCell />
+            {/* Created an empty cell to accomodate detail expansion, pushing name to the right */}
             <TableCell>Name</TableCell>
             <TableCell>Day</TableCell>
             <TableCell>Capsule</TableCell>
@@ -48,23 +56,78 @@ const UpcomingLaunches: React.FunctionComponent<UpcomingLaunchesProps> = (
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.upcoming.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              {/* TODO: Convert unix date to readable format */}
-              <TableCell align="left">{row.date}</TableCell>
-              <TableCell align="left">{row.rocket}</TableCell>
-              <TableCell align="left">{row.capsule}</TableCell>
-              <TableCell align="left">{row.launchpad}</TableCell>
-              <TableCell align="left">{row.state}</TableCell>
-            </TableRow>
+          {upcoming.map((row) => (
+            <Row key={row.name} row={row}></Row>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
 };
+
+/**
+ * React fragment specifically for the table rows so state can be maintained for detail expansion
+ * @param props
+ */
+function Row(props: { row: UpcomingLaunch }) {
+  const { row } = props;
+  // Maintain the state for each row to know if it is expanded or not
+  const [open, setOpen] = React.useState(false);
+
+  // Create a cell that modifies the state of the row based on if it's open or closed
+  const expansionCell = (
+    <TableCell>
+      <IconButton
+        aria-label="expand row"
+        size="small"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+      </IconButton>
+    </TableCell>
+  );
+
+  // Returns around fragment to prevent nested indentation when mapping the list during table creation
+  return (
+    <React.Fragment>
+      <TableRow>
+        {
+          // Only display the expansion cell if there are details otherwise return an empty cell
+          row.details !== undefined && row.details?.length > 0 ? (
+            expansionCell
+          ) : (
+            <TableCell />
+          )
+        }
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        {/* TODO: Convert unix date to readable format */}
+        <TableCell align="left">{row.date}</TableCell>
+        <TableCell align="left">{row.rocket}</TableCell>
+        <TableCell align="left">{row.capsule}</TableCell>
+        <TableCell align="left">{row.launchpad}</TableCell>
+        <TableCell align="left">{row.state}</TableCell>
+      </TableRow>
+      <TableRow>
+        {/* Set the column span to 7 because there are 7 headers for the table */}
+        {/* Remove the padding so it doesnt show a gap between rows */}
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box>
+              <p>
+                {
+                  // TODO: Create new component that will display details about the launch, if any
+                  row.details ??
+                    "Looks like there are no details for this launch"
+                }
+              </p>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
 
 export default UpcomingLaunches;
